0a7d3ea3bf2e21d49b4892a4e436aca5cc6eff19
Spencer Kinsey-Korzym <34325816+huhu72@users.noreply.github.com>
2022-10-14 19:20:48 -0400

    Initial Files

---
+++ app/src/main/java/com/example/visualizer/Dijkstras_Visualizer_Activity.java
@@ -0,0 +1,14 @@
+package com.example.visualizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dijkstras_Visualizer_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dijkstras_visualizer);
+    }
+}
