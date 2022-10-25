a1106997b20c131056c446efc0f7582a3afe1191
Spencer Kinsey-Korzym <34325816+huhu72@users.noreply.github.com>
2022-10-24 20:08:20 -0400

    Changed 2d array usage to a hashmap usage for a more efficient way of retrieving the views and modifying the settings for a view

--- app/src/main/java/com/example/visualizer/Dijkstras_Visualizer_Activity.java
+++ app/src/main/java/com/example/visualizer/Dijkstras_Visualizer_Activity.java
@@ -7,39 +7,58 @@ import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.graphics.Rect;
 import android.os.Bundle;
-import android.os.SystemClock;
-import android.util.DisplayMetrics;
-import android.util.Log;
 import android.util.TypedValue;
-import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.gridlayout.widget.GridLayout;
 
-class CustomTextViewArray{
-    TextView tv;
-    int x;
-    int y;
-    public CustomTextViewArray(TextView tv){
-        this.tv = tv;
+import java.util.HashMap;
+
+class CustomTextView {
+    TextView textView;
+    int left;
+    int right;
+    int top;
+    int bot;
+    int x, y;
+    boolean isWall = false;
+
+    public CustomTextView(TextView tv) {
+        this.textView = tv;
     }
-    public void setCord(int[] cord){
-        this.x = cord[0];
-        this.y = cord[1];
+
+    public void setRectBounds(int right, int bot) {
+        this.right = right;
+        this.bot = bot;
+    }
+
+    //Index 0 represents the x coordinate with respect to the screen since and index 1 represents y.
+    //This is needed because we are using raw x and y for our motion event
+    public void setTop(int[] offsets) {
+        this.left = offsets[0];
+        this.top = offsets[1];
+    }
+
+    public void setCord(int[] coord) {
+        this.x = coord[0];
+        this.y = coord[1];
     }
 }
+
 public class Dijkstras_Visualizer_Activity extends AppCompatActivity implements View.OnTouchListener {
 
     ConstraintLayout layout;
     ConstraintSet set = new ConstraintSet();
-    int rowSize = 25;
-    int colSize = 25;
-    CustomTextViewArray[][] grid = new CustomTextViewArray[rowSize][colSize];
+    int rowSize = 5*2;
+    int colSize = 5*2;
     int boxHeight;
     int boxWidth;
+    int xOffset, yOffset;
+    HashMap<String, CustomTextView> grid = new HashMap<>();
     GridLayout gridContainerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,17 +69,24 @@ public class Dijkstras_Visualizer_Activity extends AppCompatActivity implements 
         gridContainerView.setAlignmentMode(GridLayout.ALIGN_BOUNDS);
         gridContainerView.setColumnCount(colSize);
         gridContainerView.setRowCount(rowSize);
+        gridContainerView.setOnTouchListener(this);
         // System.out.println("Child " + gridContainerView.getChildCount());
 
         gridContainerView.post(new Runnable() {
             @Override
             public void run() {
                 System.out.println("imageview width:" + gridContainerView.getWidth() + " height:" + gridContainerView.getHeight());
-                calculateBoxSize(gridContainerView.getWidth(),gridContainerView.getHeight());
-                System.out.println(layout.getChildAt(0).getTag());
+                calculateBoxSize(gridContainerView.getWidth(), gridContainerView.getHeight());/*
+                for(int i = 0; i < layout.getChildCount(); i++){
+                    System.out.println(layout.getChildAt(i).getTag());
+                }*/
                 createGrid();
+                System.out.println("Create grid done");
+                System.out.println("container: " + gridContainerView.getY());
             }
         });
+
+/*
         DisplayMetrics displayMetrics = new DisplayMetrics();
 
         // on below line we are getting metrics for display using window manager.
@@ -73,131 +99,149 @@ public class Dijkstras_Visualizer_Activity extends AppCompatActivity implements 
 
         // on below line we are setting height and width to our text view.
         System.out.println("Width : " + String.valueOf(width) + "\n" + "Height : " + String.valueOf(height));
+*/
 
     }
 
     private void createGrid() {
 
         int counter = 1;
-        for ( int i = 0; i < rowSize; i++ ){
-            for (int j = 0; j < colSize; j++){
+        for (int i = 0; i < rowSize; i++) {
+            for (int j = 0; j < colSize; j++) {
                 TextView box = new TextView(this);
                 box.setBackgroundColor(getColor(R.color.custom_grey));
                 box.setId(TextView.generateViewId());
-                box.setTag("box-" + counter);
+                String tag = "box-" + counter;
+                box.setTag(tag);
                 box.setText(String.valueOf(counter));
                 box.setAutoSizeTextTypeUniformWithConfiguration(
                         1, 17, 1, TypedValue.COMPLEX_UNIT_DIP);
                 box.setOnTouchListener(this);
-                int[] cord = new int[2];
-                box.getLocationOnScreen(cord);
-                System.out.println("-----------------------------------------------------" + cord[0]);
-                grid[i][j] = new CustomTextViewArray(box);
                 ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(boxWidth, boxHeight);
-                if ( i == 0 && j ==0 ){
+                if (i == 0 && j == 0) {
                     //Row 0 Column  = 0
-                    layoutParams.leftMargin=getDPDimensions(0);
+                    layoutParams.leftMargin = getDPDimensions(0);
                     layoutParams.topMargin = getDPDimensions(0);
-                }else if (i != 0 && j == 0){
+                } else if (i != 0 && j == 0) {
                     //Row > 0 Column = 0
-                    layoutParams.leftMargin=getDPDimensions(0);
+                    layoutParams.leftMargin = getDPDimensions(0);
                     layoutParams.topMargin = getDPDimensions(1);
-                }else{
-                    layoutParams.leftMargin=getDPDimensions(1);
+                } else {
+                    layoutParams.leftMargin = getDPDimensions(1);
                     layoutParams.topMargin = getDPDimensions(0);
                 }
                 box.setLayoutParams(layoutParams);
-                System.out.println("Grid Child: " + gridContainerView.getChildCount());
+                //  System.out.println("Grid Child: " + gridContainerView.getChildCount());
                 gridContainerView.addView(box);
+                grid.put(tag, new CustomTextView(box));
                 counter++;
+                final int rowIndx = i;
+                final int colIndx = j;
+                grid.get(tag).textView.post(() -> {
+                    int[] coord = new int[2];
+                    grid.get(tag).textView.getLocationOnScreen(coord);
+                    grid.get(tag).setTop(coord);
+                    if (tag.equals("box-1")) {
+                        this.xOffset = coord[0];
+                        this.yOffset = coord[1];
+                    }
+                    int right = grid.get(tag).textView.getRight() + xOffset;
+                    int bot = grid.get(tag).textView.getBottom() + yOffset;
+
+                    grid.get(tag).setRectBounds(right, bot);
+/*                    if ( rowIndx == 0 && colIndx == 1){
+                        System.out.println("Creation left: " + grid.get(tag).left);
+                        System.out.println("Creation Right: " + grid.get(tag).right);
+                        System.out.println("Creation Top: " + grid.get(tag).top);
+                        System.out.println("Creation Bottom: " + grid.get(tag).bot);
+                    }*/
+                });
+
             }
 
         }
-       /* View box;
-        int counter = 1;
-        for( int i = 0; i < rowSize; i++){
-            for (int j = 0; j < colSize; j++){
-                box = new View(this);
-                box.setId(View.generateViewId());
-                box.setBackgroundColor(getResources().getColor(R.color.custom_grey));
-                box.setTag("box-" + counter);
-                box.setLayoutParams(new ConstraintLayout.LayoutParams(80,68));
-                layout.addView(box);
-                grid[i][j] = box;
-                if ( i == 0 && j ==0 ){
-                    //Row 0 Column  = 0
-                    set.connect(box.getId(), ConstraintSet.LEFT, layout.getChildAt(0).getId(), ConstraintSet.LEFT, 0);
-                    set.connect(box.getId(), ConstraintSet.TOP, layout.getChildAt(0).getId(), ConstraintSet.TOP, 0);
-                }else if (i != 0 && j == 0){
-                    //Row > 0 Column = 0
-                    set.connect(box.getId(), ConstraintSet.LEFT, grid[i-1][j].getId(), ConstraintSet.LEFT, 0);
-                    set.connect(box.getId(), ConstraintSet.TOP, grid[i-1][j].getId(), ConstraintSet.BOTTOM, getDPDimensions(1));
-                }else{
-                    set.connect(box.getId(), ConstraintSet.LEFT, grid[i][j-1].getId(), ConstraintSet.RIGHT, getDPDimensions(1));
-                    set.connect(box.getId(), ConstraintSet.TOP, grid[i][j-1].getId(), ConstraintSet.TOP, 0);
-                }
-                set.constrainWidth(box.getId(), boxWidth);
-                set.constrainHeight(box.getId(), boxHeight);
-                set.applyTo(layout);
-                counter ++;
+ /*       for( int i = 0; i < rowSize; i ++){
+            for( int j = 0; j< colSize; j++){
+                final int rowIndx = i;
+                final int colIndx = j;
+                grid[rowIndx][colIndx].textView.post(() -> {
+                    int[] coord = new int[2];
+                    grid[rowIndx][colIndx].textView.getLocationOnScreen(coord);
+                    grid[rowIndx][colIndx].setCord(coord);
+                    System.out.println(grid[0][0].y);
+                });
             }
-        }
+        }*/
+/*        for( CustomTextView ctv[] : grid ){
+            for( CustomTextView tv: ctv){
+                int coord[] = new int[2];
+                tv.textView.getLocationOnScreen(coord);
+                tv.setCord(coord);
+                System.out.println(tv.textView.getTag());
+                System.out.println("For each loop x: " + tv.x);
+                System.out.println("For each loop y: " + tv.y);
+            }
+        }*/
+/*        grid[1][0].textView.post(() -> {
+            int[] coord = new int[2];
+            grid[1][0].textView.getLocationOnScreen(coord);
+            grid[1][0].setCord(coord);
+            System.out.println("Runnable X: " + grid[1][0].x);
+            System.out.println("Runnable y: " + grid[1][0].y);
+        });*/
+        /*System.out.println("view x : " + grid[0][1].x);
+        System.out.println("view y : " + grid[0][1].y);*/
+   /*     grid[0][0].textView.post(() -> {
+            int left = grid[0][0].textView.getLeft();
+            int right = grid[0][0].textView.getRight();
+            int top = grid[0][0].textView.getTop();
+            int bot = grid[0][0].textView.getBottom();
+            grid[0][1].setRectBounds(left,top,right,bot);
+                System.out.println("Runnable left: " + grid[0][0].textView.getLeft());
+                System.out.println("Runnable Right: " + grid[0][0].textView.getRight());
+                System.out.println("Runnable Top: " + grid[0][0].textView.getTop());
+                System.out.println("Runnable Bottom: " + grid[0][0].textView.getBottom());
+        });
 */
     }
 
     private void calculateBoxSize(int containerWidth, int containerHeight) {
-        int totalWidthSpacing = (rowSize-1)*getDPDimensions(1);
+        int totalWidthSpacing = (rowSize - 1) * getDPDimensions(1);
         System.out.println("total Width spacing: " + totalWidthSpacing);
-        boxWidth = (containerWidth-totalWidthSpacing)/rowSize;
+        boxWidth = (containerWidth - totalWidthSpacing) / rowSize;
         System.out.println("box Width : " + boxWidth);
-        int totalHeightSpacing = (colSize-1)*getDPDimensions(1);
-        boxHeight = (containerHeight-totalHeightSpacing)/colSize;
+        int totalHeightSpacing = (colSize - 1) * getDPDimensions(1);
+        boxHeight = (containerHeight - totalHeightSpacing) / colSize;
+        System.out.println("box Height : " + boxHeight);
     }
 
-    private int getDPDimensions(int value){
-        return (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value, getResources().getDisplayMetrics());
+    private int getDPDimensions(int value) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value, getResources().getDisplayMetrics());
     }
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_DOWN){
-            System.out.println(v.getTag() + " preesed");
-            for( CustomTextViewArray ctv[] : grid){
-                for( CustomTextViewArray CTV: ctv){
-                    int cord[] = new int[2];
-                    CTV.tv.getLocationOnScreen(cord);
-                    CTV.setCord(cord);
-                }
-            }
-            v.setBackgroundColor(getColor(R.color.white));
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            CustomTextView box = grid.get(v.getTag());
+            changeBoxColor(box);
         }
-        if (event.getAction() == MotionEvent.ACTION_MOVE){
-            System.out.println(v.getTag() + " moved");
-            System.out.println(event.getRawX());
-            TextView hoveredView;
-            Rect r1 = new Rect((int)event.getRawX(),(int)event.getRawY(),1,1);
-            //v.dispatchTouchEvent(MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), (int)MotionEvent.ACTION_CANCEL, 0, 0, 0));
-            for(CustomTextViewArray[] TV: grid){
-                for(CustomTextViewArray tv: TV){
-                    if( r1.intersect(tv.x,tv.y,1,1)){
-                        System.out.println(r1.intersect(tv.x,tv.y,1,1));
-                    }
+        if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            grid.forEach((key, box) -> {
+                Rect r = new Rect(box.left, box.top, box.right, box.bot);
+                if (r.contains((int) event.getRawX(), (int) event.getRawY()) && v.getTag() != box.textView.getTag()) {
+                    changeBoxColor(box);
                 }
-            }
-            System.out.println("called up");
-
+            });
         }
-        if (event.getAction() == MotionEvent.ACTION_UP){
-            System.out.println(v.getTag() + " released");
-     /*      for(TextView[] TV: grid){
-               for(TextView tv: TV){
-                    System.out.println(tv.getTag());
-               }
-           }*/
-
-            System.out.println(grid[0][1].x);
+        if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            System.out.println(event.getX() + "         " + event.getRawY());
         }
         return true;
+    }
+    public void changeBoxColor(CustomTextView box){
+        box.textView.setBackgroundColor(getColor(R.color.white));
+        box.isWall = true;
+        grid.put(box.textView.getTag().toString(), box);
     }
 }
 /*
